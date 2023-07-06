c60cbe9859326c75f22ef6f1ceaf9e30b82afb21
Abhishek-Gurav <79034352+Abhishek-Gurav@users.noreply.github.com>
2023-07-06 20:09:20 +0530

    Update Hands.js

--- client/src/components/Hands.js
+++ client/src/components/Hands.js
@@ -1,15 +1,19 @@
-import{ Hands} from "@mediapipe/hands";
-import React, { useRef, useEffect } from "react";
-import * as Hand from "@mediapipe/hands";
-import * as cam from "@mediapipe/camera_utils";   
+import * as cam from "@mediapipe/camera_utils";
+import { Hands } from "@mediapipe/hands";
+import React, { useEffect, useRef, useState } from "react";
+import { isTablet } from "react-device-detect";
 import Webcam from "react-webcam";
-import { isTablet } from "react-device-detect"; 
-import "./facemask.css"
 import tattoo from "../Images/Tattoo.png";
+import "./facemask.css";
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
-  var connect =  window.drawConnectors;
+  const [windowDimention, detectHW] = useState({
+    winWidth: window.innerWidth,
+    winHeight: window.innerHeight,
+  });
+  const [isTab, setTab] = React.useState(false);
+  // var connect = window.drawConnectors;
   function onResults(results) {
     // const video = webcamRef.current.video;
     const videoWidth = webcamRef.current.video.videoWidth;
@@ -33,28 +37,35 @@ function App() {
     if (results.multiHandLandmarks) {
       var img = new Image();
       // img.src = "https://www.svgrepo.com/show/62101/sword.svg";
-      img.src = "https://www.onlygfx.com/wp-content/uploads/2022/04/spider-and-web-tattoo-2611.svg"
+      img.src =
+        "https://www.onlygfx.com/wp-content/uploads/2022/04/spider-and-web-tattoo-2611.svg";
       // console.log(results.multiHandLandmarks.x);
       for (const landmarks of results.multiHandLandmarks) {
-            var l = 0
-            var k = 0
-            for(const landmark of landmarks){
-              l += landmark.x;
-              k += landmark.y;
-            }
-            l = l/(landmarks.length);
-            k = k/(landmarks.length);
-            canvasCtx.drawImage(img, Math.round(l*640)-50, Math.round(k*480),100, 100);
-            // canvasCtx.drawImage(img, Math.round(((landmarks[5].x))*640)-150, Math.round((landmarks[5].y)*480)-150,100, 100);
+        var l = 0;
+        var k = 0;
+        for (const landmark of landmarks) {
+          l += landmark.x;
+          k += landmark.y;
+        }
+        l = l / landmarks.length;
+        k = k / landmarks.length;
+        canvasCtx.drawImage(
+          img,
+          Math.round(l * 640) - 50,
+          Math.round(k * 480),
+          100,
+          100
+        );
+        // canvasCtx.drawImage(img, Math.round(((landmarks[5].x))*640)-150, Math.round((landmarks[5].y)*480)-150,100, 100);
 
-            // console.log(landmarks);
+        // console.log(landmarks);
       }
     }
     canvasCtx.restore();
   }
   const w = isTablet ? "70vw" : "45vw";
   useEffect(() => {
-    // Face mesh 
+    // Face mesh
     const hand = new Hands({
       locateFile: (file) => {
         return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
@@ -62,11 +73,11 @@ function App() {
     });
 
     hand.setOptions({
-        maxNumHands: 2,
-        modelComplexity: 1,
-        minDetectionConfidence: 0.5,
-        minTrackingConfidence: 0.5
-      });
+      maxNumHands: 2,
+      modelComplexity: 1,
+      minDetectionConfidence: 0.5,
+      minTrackingConfidence: 0.5,
+    });
 
     hand.onResults(onResults);
 
@@ -85,54 +96,82 @@ function App() {
       camera.start();
     }
   }, []);
+  useEffect(() => {
+    const handleResize = () => {
+      detectHW({
+        winWidth: window.innerWidth,
+        winHeight: window.innerHeight,
+      });
+    };
+    if (windowDimention.winWidth < 992) {
+      setTab(true);
+    } else {
+      setTab(false);
+    }
+    console.log(windowDimention.winWidth);
+    console.log(isTab);
+    
+    window.addEventListener('resize', handleResize);
+    
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [windowDimention]);
   return (
-    <center>
-      <div className="face__div" style={{position:"relative"}}>
-      <div className="face__text">
-        <h1 className="face__head">
-          Hand Tattoo
-        </h1>
-        <p className="face__para">
-          Move your hand in front of the camera and see the spider tattoo floating.
-        </p>
-        <img width="90%" style={{marginLeft:"-20px"}} src={tattoo} alt="ghost"></img>
-      </div>
-      <div className="face__canvas">
-        <Webcam
-          ref={webcamRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            right: "5vw",
-            textAlign: "center",
-            zindex: 9,
-            marginTop:"8vw",
-            width: w,
-            height:"auto",
-            borderRadius:"10%",
-            border:"10px solid black"
-          }}
-        />
-        <canvas
-          ref={canvasRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            right: "5vw",
-            textAlign: "center",
-            zindex: 9,
-            marginTop:"8vw",
-            width:w,
-            height:"auto",
-            borderRadius:"10%",
-            border:"10px solid black"
-          }}
-        ></canvas>
+      <div className="d-flex flex-wrap face__div" style={{ 
+        position: "relative",
+        justifyContent: "space-around",
+        }}>
+        <div className="face__text">
+          <h1 className="face__head">Hand Tattoo</h1>
+          <p className="face__para">
+            Move your hand in front of the camera and see the spider tattoo
+            floating.
+          </p>
+          <img
+            width="90%"
+            style={{ marginLeft: "-20px" }}
+            src={tattoo}
+            alt="tatoo"
+          ></img>
+        </div>
+        <div className="face__canvas" style={{
+          position : "relative",
+
+        }}>
+          <Webcam
+            ref={webcamRef}
+            style={{
+              position: "relative",
+              marginLeft: "auto",
+              marginRight: "auto",
+              textAlign: "center",
+              zindex: 9,
+              marginTop: "10vh",
+              width: isTab ? "80vw" : "45vw",
+              height: "auto",
+              borderRadius: "10%",
+              border: "10px solid black",
+            }}
+          />
+          <canvas
+            ref={canvasRef}
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              right: isTab ? "0vw" : "0vw",
+              textAlign: "center",
+              zindex: 9,
+              marginTop: "10vh",
+              width: isTab ? "80vw" : "45vw",
+              height: "auto",
+              borderRadius: "10%",
+              border: "10px solid black",
+            }}
+          ></canvas>
         </div>
       </div>
-    </center>
   );
 }
 
