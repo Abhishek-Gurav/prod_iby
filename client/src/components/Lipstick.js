39277eb8dfc204d50798116cb69aee258fd5f0f8
Abhishek-Gurav <79034352+Abhishek-Gurav@users.noreply.github.com>
2023-07-06 20:24:43 +0530

    Update Lipstick.js

--- client/src/components/Lipstick.js
+++ client/src/components/Lipstick.js
@@ -1,14 +1,17 @@
-import { FaceMesh } from "@mediapipe/face_mesh";
-import React, { useRef, useEffect } from "react";
+import * as cam from "@mediapipe/camera_utils";
 import * as Facemesh from "@mediapipe/face_mesh";
-import * as cam from "@mediapipe/camera_utils";   
+import { FaceMesh } from "@mediapipe/face_mesh";
+import React, { useEffect, useRef, useState } from "react";
 import Webcam from "react-webcam";
-import { isTablet } from "react-device-detect";
 import lipstick from "../Images/Lipstick.png";
 function Lips() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
-  var connect =  window.drawConnectors;
+  const [windowDimention, detectHW] = useState({
+    winWidth: window.innerWidth,
+    winHeight: window.innerHeight,
+  });
+  const [isTab, setTab] = React.useState(false);
   function onResults(results) {
     var img = new Image();
     img.src = "https://www.svgrepo.com/show/62101/sword.svg";
@@ -29,38 +32,40 @@ function Lips() {
     canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
     function fillPolygon(points, color) {
       if (points.length > 0) {
-          canvasCtx.fillStyle = color; // all css colors are accepted by this property
+        canvasCtx.fillStyle = color; // all css colors are accepted by this property
 
-          var point = points[0];
+        var point = points[0];
 
-          canvasCtx.beginPath();
-          canvasCtx.moveTo(point.X+7, point.Y);   // point 1
+        canvasCtx.beginPath();
+        canvasCtx.moveTo(point.X + 7, point.Y); // point 1
 
-          for (var i = 1; i < points.length; ++i) {
-              point = points[i];
+        for (var i = 1; i < points.length; ++i) {
+          point = points[i];
 
-              canvasCtx.lineTo(point.X+7, point.Y);
-          }
-          canvasCtx.closePath();      // go back to point 1
-          canvasCtx.fill();
+          canvasCtx.lineTo(point.X + 7, point.Y);
+        }
+        canvasCtx.closePath(); // go back to point 1
+        canvasCtx.fill();
       }
-  }
+    }
 
     if (results.multiFaceLandmarks) {
       for (const landmarks of results.multiFaceLandmarks) {
         const points = [];
         // console.log(Facemesh.FACEMESH_LEFT_EYE);
-        for(const a of Facemesh.FACEMESH_LIPS){
-          points.push({X: landmarks[a[0]].x * videoWidth, Y: landmarks[a[1]].y * videoHeight});
+        for (const a of Facemesh.FACEMESH_LIPS) {
+          points.push({
+            X: landmarks[a[0]].x * videoWidth,
+            Y: landmarks[a[1]].y * videoHeight,
+          });
         }
         fillPolygon(points, "#9f1f19");
       }
     }
     canvasCtx.restore();
   }
-  const w = isTablet ? "70vw" : "45vw";
   useEffect(() => {
-    // Face mesh 
+    // Face mesh
     const faceMesh = new FaceMesh({
       locateFile: (file) => {
         return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
@@ -91,32 +96,66 @@ function Lips() {
       camera.start();
     }
   }, []);
+  useEffect(() => {
+    const handleResize = () => {
+      detectHW({
+        winWidth: window.innerWidth,
+        winHeight: window.innerHeight,
+      });
+    };
+    if (windowDimention.winWidth < 992) {
+      setTab(true);
+    } else {
+      setTab(false);
+    }
+    console.log(windowDimention.winWidth);
+    console.log(isTab);
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [windowDimention]);
   return (
-    <center>
-      <div className="face__div" style={{position:"relative"}}>
+    <div
+      className="d-flex flex-wrap face__div"
+      style={{
+        position: "relative",
+        justifyContent: "space-around",
+      }}
+    >
       <div className="face__text">
-        <h1 className="face__head">
-          Lipstick
-        </h1>
+        <h1 className="face__head">Lipstick</h1>
         <p className="face__para">
           Try on different shades of Lipstick on your face.
         </p>
-        <img width="90%" style={{marginLeft:"-20px"}} src={lipstick} alt="ghost"></img>
+        <img
+          width="90%"
+          style={{ marginLeft: "-20px" }}
+          src={lipstick}
+          alt="ghost"
+        ></img>
       </div>
+      <div
+        className="face__canvas"
+        style={{
+          position: "relative",
+        }}
+      >
         <Webcam
           ref={webcamRef}
           style={{
-            position: "absolute",
+            position: "relative",
             marginLeft: "auto",
             marginRight: "auto",
-            right: "5vw",
             textAlign: "center",
             zindex: 9,
-            marginTop:"8vw",
-            width: w,
-            height:"auto",
-            borderRadius:"10%",
-            border:"10px solid black"
+            marginTop: "10vh",
+            width: isTab ? "80vw" : "45vw",
+            height: "auto",
+            borderRadius: "10%",
+            border: "10px solid black",
           }}
         />
         <canvas
@@ -125,18 +164,18 @@ function Lips() {
             position: "absolute",
             marginLeft: "auto",
             marginRight: "auto",
-            right: "5vw",
+            right: isTab ? "0vw" : "0vw",
             textAlign: "center",
             zindex: 9,
-            marginTop:"8vw",
-            width: w,
-            height:"auto",
-            borderRadius:"10%",
-            border:"10px solid black"
+            marginTop: "10vh",
+            width: isTab ? "80vw" : "45vw",
+            height: "auto",
+            borderRadius: "10%",
+            border: "10px solid black",
           }}
         ></canvas>
       </div>
-    </center>
+    </div>
   );
 }
 
