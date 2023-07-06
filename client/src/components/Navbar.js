cfc27392aaf5b2b1f6444587d23273b335c2f077
Abhishek-Gurav <79034352+Abhishek-Gurav@users.noreply.github.com>
2023-07-06 20:25:26 +0530

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,53 +1,74 @@
-import React, {useState} from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import "./navbar.css";
-import logo from '../Images/Logo.jpeg';
-import logout from '../Images/logout.png';
-
-function NavScrollExample() {
-
+import Offcanvas from "react-bootstrap/Offcanvas";
+import logo from "../Images/Logo.jpeg";
+import logout from "../Images/logout.png";
+function OffcanvasExample() {
   return (
-    <Navbar className='nav_main' expand="lg">
-      <Container fluid>
-      <Navbar.Brand className='d-flex' href="/">
-            <img
-              src={logo}
-              width="35"
-              height="35"
-              className="d-inline-block align-top logo"
-              alt="Filter Pic"
-            />{' '}
-            <span className='nav_head'>Filter Cam</span>
-          </Navbar.Brand>
-        <Navbar.Toggle
-         aria-controls="navbarScroll"
-          />
-        <Navbar.Collapse className='Nav_c' id="navbarScroll">
-          <Nav
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link className="nav_link" href="/ghostmask">Ghostmask</Nav.Link>
-            <Nav.Link className="nav_link" href="/hand">Hand-Tattoo</Nav.Link>
-            <Nav.Link className="nav_link" href="/lipstick">Lipstick</Nav.Link>
-            <Nav.Link className="nav_link" href="/nlp">Text Analyzer</Nav.Link>
-            <Nav.Link className="nav_link"  href="/logout">
-            <img
-              src={logout}
-              width="25"
-              height="25"
-              className="d-inline-block "
-              alt="logout"
-              style={{marginTop: '-3px'}}
-            />
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      {["lg"].map((expand) => (
+        <Navbar
+          key={expand}
+          expand={expand}
+          className="nav_main bg-body-tertiary mb-3"
+        >
+          <Container fluid>
+            <Navbar.Brand className="d-flex" href="#">
+              <a href="/" style={{ textDecoration: "none"}}>
+                <img
+                  src={logo}
+                  width="35"
+                  height="35"
+                  className="d-inline-block align-top logo"
+                  alt="Filter Pic"
+                />{" "}
+                <span className="nav_head">Filter Cam</span>
+              </a>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Body className="Nav_c">
+                <Nav
+                  style={{ maxHeight: "100px" }}
+                  className="justify-content-lg-end flex-grow-1 pe-3"
+                >
+                  <Nav.Link className="nav_link" href="/ghostmask">
+                    Ghostmask
+                  </Nav.Link>
+                  <Nav.Link className="nav_link" href="/hand">
+                    Hand-Tattoo
+                  </Nav.Link>
+                  <Nav.Link className="nav_link" href="/lipstick">
+                    Lipstick
+                  </Nav.Link>
+                  <Nav.Link className="nav_link" href="/nlp">
+                    Text Analyzer
+                  </Nav.Link>
+                  <Nav.Link className="nav_link" href="/logout">
+                    <img
+                      src={logout}
+                      width="25"
+                      height="25"
+                      className="d-inline-block "
+                      alt="logout"
+                      style={{ marginTop: "-3px" }}
+                    />
+                  </Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
   );
 }
 
-export default NavScrollExample;
+export default OffcanvasExample;
+
